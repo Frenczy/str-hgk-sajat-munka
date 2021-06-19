ae8846dc924e75187889954bd6803c4bbbf88398
Frenczy <frenczypp@gmail.com>
2021-06-19 13:59:52 +0200

    Házi feladat

--- nodejs-alapjai-fs-modul/2.feladat/utils.js
+++ nodejs-alapjai-fs-modul/2.feladat/utils.js
@@ -3,10 +3,10 @@ const { createReadStream, createWriteStream } = require('fs')
 const { unlink } = require('fs').promises
 
 const copyFile = function (filePath) {
-    createReadStream('filePath', {encoding:'utf8', highWaterMark: 1024}).pipe(createWriteStream(`${filePath}.bak`))}
+    createReadStream(filePath, {encoding:'utf8', highWaterMark: 1024}).pipe(createWriteStream(`${filePath}.bak`))}
 const zipFile = function (filePath) {
     createReadStream(`${filePath}.bak`, {encoding:'utf8', highWaterMark: 1024}).pipe(createGzip()).pipe(createWriteStream(`${filePath}.bak.gz`))}
-const deleteFile = function (filePath){unlink(filePath, error=>{if(error) throw (error)})}
+const deleteFile = function (filePath){unlink(filePath, error=>{if(error) throw (error)}); unlink(`${filePath}.bak`, error=>{if(error) throw (error)})}
 
 module.exports= {
 copyFile,
--- nodejs-alapjai-module-system/2.feladat/index.js
+++
@@ -1,6 +0,0 @@
-const functions = require('./utils')
-
-const users=[{firstName:"John", lastName:"Doe", age:30},{firstName:"Jane", lastName:"Doe", age:15}]
-
-console.log(functions.generateUserList(users))
-console.log(functions.getUserNames(users))
